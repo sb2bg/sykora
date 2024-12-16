91a3cc7ec857b445740b59afe03b8be3eababf2e
sb2bg <sullivanbognar@gmail.com>
2024-12-16 02:29:05 -0600

    Add algebraicToBitboard and makeMove functions to BitBoard struct

--- src/board/bitboard.zig
+++ src/board/bitboard.zig
@@ -4,5 +4,11 @@ const UciError = @import("../uci/uci_error.zig").UciError;
 pub const BitBoard = struct {
     const Self = @This();
 
+    pub fn algebraicToBitboard(_: Self, _: []const u8) UciError!u64 {
+        return 0;
+    }
+
+    pub fn makeMove(_: Self, _: u64) UciError!void {}
+
     pub fn reset(_: Self) UciError!void {}
 };
