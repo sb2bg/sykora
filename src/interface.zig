16758b28f32a85e0c170f0ce9b5a8877ce9a9e9f
sb2bg <sullivanbognar@gmail.com>
2026-03-02 22:58:48 -0600

    Update version

--- src/interface.zig
+++ src/interface.zig
@@ -18,7 +18,7 @@ const nnue = @import("nnue.zig");
 
 const name = "Sykora";
 const author = "Sullivan Bognar";
-const version = "0.2.1";
+const version = "0.2.2";
 
 pub const Uci = struct {
     const Self = @This();
